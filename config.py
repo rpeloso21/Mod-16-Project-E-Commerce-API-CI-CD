cf5c6cbb4387a4a3785c684b2b1ae1cc674209aa
rpeloso21 <rpeloso21@gmail.com>
2024-12-14 13:14:18 -0500

    adding new database

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class DevelopmentConfig:
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:Tsrost007!@localhost/advanced_e_commerce_db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://example_sum_postgres_mtk8_user:XagGn67OROsXKqFqeTgJtPgeawzy0hBE@dpg-ct6pra1u0jms739b2mag-a.oregon-postgres.render.com/example_sum_postgres_mtk8'
     CACHE_TYPE = "SimpleCache"
     DEBUG = True
